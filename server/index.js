5bbb48aa2c58a8fd4eec8ac26ac242bb932ad9fe
Adnan Zaib <adnanzaib6@gmail.com>
2023-12-13 12:05:12 +0500

    First and Final commit

---
+++ server/index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
+// Config
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// Storage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+// Routes
+app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+// DB
+const PORT = process.env.port || 3001;
+// mongoose.set("useNewUrlParser", true);
+// mongoose.set("useUnifiedTopology", true);
+// mongoose
+//   .connect(process.env.DB_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("DB Connected and Server Started on Port => ", PORT)
+    );
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+  })
+  .catch((err) => console.log(`${err} unable to connect`));
---
+++ server/routes/posts.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { verifyToken } from "../middleware/auth.js";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+
+const router = express.Router();
+
+router.get("/", verifyToken, getFeedPosts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
